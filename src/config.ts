9130430af16e41313e027dd547585aba5eeeb374
Youzix_Star <166191037+xiexingssb@users.noreply.github.com>
2026-02-03 13:29:15 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -23,9 +23,9 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
-	subtitle: "One demo website",
+	subtitle: "Youzix！",
 	siteURL: "https://youzix.dpdns.org/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-02-03", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-02-03", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
